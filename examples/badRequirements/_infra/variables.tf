6c608ae5deb4d6b30eb6a3d11961e5514f79bae3
Stacey Carroll <stacey.v.carroll@gmail.com>
2023-10-01 22:02:26 -0500

    Feat: add draft of examples

---
+++ examples/badRequirements/_infra/variables.tf
@@ -0,0 +1,7 @@
+variable "mock_aws_parameter_paths" {
+  description = "An example of how paths to AWS parameter store values may be passed via terraform"
+  type = object({
+    param  = optional(string, "/example/path/param")
+    secret = optional(string, "/example/path/secret")
+  })
+}
